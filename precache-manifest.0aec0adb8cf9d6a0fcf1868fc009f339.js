ad3c702866d0c40f31c7046a04bf9fc39ae47b44
jamisonwight <jmsnwight@gmail.com>
2021-08-04 17:22:17 -0500

    deploy

---
+++ precache-manifest.0aec0adb8cf9d6a0fcf1868fc009f339.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462c4076d300b4749f58",
+    "url": "/wine-talks-pwa/css/app.b736d713.css"
+  },
+  {
+    "revision": "7d51cee0595d49a0a033",
+    "url": "/wine-talks-pwa/css/chunk-vendors.6a8ffaba.css"
+  },
+  {
+    "revision": "ce842fa16f1a9b34fc5a96b34cf6f4cd",
+    "url": "/wine-talks-pwa/img/arrow-left-dark-icon.ce842fa1.svg"
+  },
+  {
+    "revision": "9b68a198fd3fd14f7bf3c6bff83588c6",
+    "url": "/wine-talks-pwa/img/arrow-right-icon.9b68a198.svg"
+  },
+  {
+    "revision": "8422c01e6035115335c5607085780f63",
+    "url": "/wine-talks-pwa/img/background.8422c01e.svg"
+  },
+  {
+    "revision": "9ec8649fc4c29c70fd4eaa253245e09b",
+    "url": "/wine-talks-pwa/img/date-night-icon.9ec8649f.svg"
+  },
+  {
+    "revision": "53d17f456ca217e2f2c08b99c27d41c0",
+    "url": "/wine-talks-pwa/img/date-night-yellow-icon.53d17f45.svg"
+  },
+  {
+    "revision": "3f4ce9b202430e2d419614ffb65fa296",
+    "url": "/wine-talks-pwa/img/family-friendly-icon.3f4ce9b2.svg"
+  },
+  {
+    "revision": "85049798df8a90d7c1af5cfbbeaf85e7",
+    "url": "/wine-talks-pwa/img/family-friendly-yellow-icon.85049798.svg"
+  },
+  {
+    "revision": "37af862392f4273d2d8a6dac1d6ff54e",
+    "url": "/wine-talks-pwa/img/girls-night-icon.37af8623.svg"
+  },
+  {
+    "revision": "8702590434d1f1f9821dcb5b1d7139c9",
+    "url": "/wine-talks-pwa/img/girls-night-yellow-icon.87025904.svg"
+  },
+  {
+    "revision": "d8da924566292dbcc0f46bc17f4a4984",
+    "url": "/wine-talks-pwa/img/heart-black.d8da9245.svg"
+  },
+  {
+    "revision": "539e61b8e4517f1cc867c7e14a5c8a32",
+    "url": "/wine-talks-pwa/img/heart-red.539e61b8.svg"
+  },
+  {
+    "revision": "ce5e36329cf11fb69cee47cd723bf9de",
+    "url": "/wine-talks-pwa/img/home-icon.ce5e3632.svg"
+  },
+  {
+    "revision": "cff62bec3592a15a821861621bcd3aad",
+    "url": "/wine-talks-pwa/img/logo.cff62bec.svg"
+  },
+  {
+    "revision": "6ac25e2fae2a4a4c1d63590d69f58db9",
+    "url": "/wine-talks-pwa/index.html"
+  },
+  {
+    "revision": "462c4076d300b4749f58",
+    "url": "/wine-talks-pwa/js/app.6b9f27e4.js"
+  },
+  {
+    "revision": "7d51cee0595d49a0a033",
+    "url": "/wine-talks-pwa/js/chunk-vendors.0a0ef38c.js"
+  },
+  {
+    "revision": "63a1bb77c33d272feb137fc2f09ae752",
+    "url": "/wine-talks-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wine-talks-pwa/robots.txt"
+  }
+]);
